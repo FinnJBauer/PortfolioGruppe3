3c401528622c8926656c2d6c02b68b0eae4a6c10
Soufiane461 <79450257+Soufiane461@users.noreply.github.com>
2021-06-07 10:34:40 +0200

    Testprojekt Mathis
    Testprojekt

---
+++ View/src/View.java
@@ -0,0 +1,6 @@
+public class View {
+    public static void main(String[] args) {
+        ViewForm viewform = new ViewForm();
+        viewform.setVisible(true);
+    }
+}
---
+++ View/src/ViewForm.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class ViewForm extends JFrame {
+    private JPanel rootPanel;
+    private JButton suchenButton;
+    private JComboBox comboBox1;
+    private JFormattedTextField formattedTextField1;
+    private JFormattedTextField formattedTextField2;
+
+    public ViewForm(){
+        add(rootPanel);
+
+        setTitle("Aufgabe 3: Postleitzahlen und Länder");
+        setSize(400, 500);
+    }
+
+
+}
